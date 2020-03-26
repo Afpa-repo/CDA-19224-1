3c77c6224bc3fde2102eef4051c6a74630a0691c
Julien-Broyard <broyard.dev@gmail.com>
2020-03-26 16:15:01 +0100

    :sparkles: Add postcss-loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,4 @@
 // Imports materialize-css js files
-import "materialize-css/dist/js/materialize"
+import "materialize-css/dist/js/materialize";
+// Imports custom CSS files
+import '../css/app.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,42 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
-    // directory where compiled assets will be stored
+    // Directory where compiled assets will be stored.
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
+    // Public path used by the web server to access the output path.
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
-
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
+    // Add 1 entry for each "page" of your app.
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
-
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
+    // Helps Webpack do it's job for multiple entry files.
     .enableSingleRuntimeChunk()
-
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
+    // Removes the old files before building the new ones.
     .cleanupOutputBeforeBuild()
+    // Displays notifications.
     .enableBuildNotifications()
+    // Add sources maps for debugging.
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
+    // Enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-
     // enables @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
+    .configureBabelPresetEnv(config => {
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
-
-    // enables Sass/SCSS support
-    //.enableSassLoader()
-
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
-
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-
-    // uncomment if you use API Platform Admin (composer req api-admin)
-    //.enableReactPreset()
-    //.addEntry('admin', './assets/js/admin.js')
+    // Gets integrity="..." attributes on your script & link tags
+    .enableIntegrityHashes(Encore.isProduction())
+    // Enables and loads PostCSS
+    .enablePostCssLoader(options => {
+        options.config = {
+            path: 'postcss.config.js'
+        }
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();

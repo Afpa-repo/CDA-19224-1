772a44fafcf3228d92c0339392b598d574d9f1d3
CocoCouq <couq@icloud.com>
2020-03-29 13:51:43 +0200

    add routes history

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -14,6 +14,17 @@ class indexController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('pages/index.html.twig');
+        // Define Routes to display the current location
+        // The 'breadcrumb' Materialize is not required
+        /* If you want use this, you have to inject precisely
+        'history_route' to the base.html contained in all pages */
+        $Routes = [
+            'Accueil' => '/',
+            'Test' => '/login_check'
+        ];
+
+        return $this->render('pages/index.html.twig', [
+            'history_routes' => $Routes
+        ]);
     }
 }

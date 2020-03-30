938add20814cde6d35bdd754fdb901c44f5bfbb8
arnaud567 <50096398+arnaud567@users.noreply.github.com>
2020-03-30 10:07:23 +0200

    Modif entité category

--- src/Entity/Ct404Category.php
+++ src/Entity/Ct404Category.php
@@ -21,9 +21,44 @@ class Ct404Category
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $categoryName;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Ct404Category")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $idCategory;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getCategoryName(): ?string
+    {
+        return $this->categoryName;
+    }
+
+    public function setCategoryName(string $categoryName): self
+    {
+        $this->categoryName = $categoryName;
+
+        return $this;
+    }
+
+    public function getIdCategory(): ?self
+    {
+        return $this->idCategory;
+    }
+
+    public function setIdCategory(?self $idCategory): self
+    {
+        $this->idCategory = $idCategory;
+
+        return $this;
     }
 
 

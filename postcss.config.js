863b5351ee72f66e2b2605ec6e0c7959c2f93513
Julien-Broyard <broyard.dev@gmail.com>
2020-04-04 16:07:12 +0200

    :wrench: Migrate styleint config in his own file

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     plugins: [
-        require('stylelint')({
-            extends: ['stylelint-config-standard'],
-            rules: {indentation: 4}
-        }),
+        require('stylelint')(),
         require('postcss-preset-env')({stage: 3}),
         require('postcss-font-magician')({display: 'swap'}),
         require('cssnano')({preset: 'default'}),

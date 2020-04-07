a0156c137607e33135eb4032f41917cc179a60c1
arnaud567 <50096398+arnaud567@users.noreply.github.com>
2020-04-07 14:58:35 +0200

    ajustement du dropdown 'quantité' du panier

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,12 +1,18 @@
 import '../css/cart.css';
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdownn").classList.toggle("show");
-  }
-  document.getElementById("dropbtn1").addEventListener("click", myFunction); 
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+// Récupère le nombre de produit (span avec id compteur)
+var compteur = parseInt(document.getElementById("compteur").textContent);
+// Si la valeur récupérée est un nombre
+if (typeof(compteur) == "number"){
+    //On boucle un event qui toggle la classe show du dropdown lors d'un click sur le bouton correspondant, et cela pour tous les produits
+    for (let x = 1; x <= compteur; x++) {
+          document.getElementById("dropbtn"+x+"").addEventListener("click", function(){
+            document.getElementById("myDropdownn"+x+"").classList.toggle("show");
+          }); 
+          
+      }
+}
+// Close any dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdownn-content");
       var i;
